f95d9fe31387ba98c5f8f40a91d33f3bb1551c8b
Krishnan Subramanian <krishnansubramanian007@gmail.com>
2023-06-19 12:51:19 +0530

    renamed file

---
+++ tests/calendar.test.ts
@@ -0,0 +1,21 @@
+import {expect, test} from "@playwright/test"
+import moment from "moment"
+
+test("validate calender tests",async ({page}) => {
+
+    page.goto("https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo")
+    let date = "2016-05-03";
+
+    await page.fill("id=birthday", date)
+})
+
+test("validate start date and end date",async ({page}) => {
+
+    page.goto("https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo")
+    await page.locator("//input[@placeholder='Start date']").click();
+
+    //const thisMonth = moment(dateToSelect,"MMMM YYYY").isBefore()
+    //console.log("this month ? " + thisMonth)
+
+
+})
