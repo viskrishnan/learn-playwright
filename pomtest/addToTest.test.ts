2063402f3e6c62bba15657991b1eb43cf98a995a
Krishnan Subramanian <krishnansubramanian007@gmail.com>
2023-06-20 15:31:15 +0530

    modified config file for URL

--- pomtest/addToTest.test.ts
+++ pomtest/addToTest.test.ts
@@ -1,2 +1,14 @@
 import {test} from "@playwright/test"
 
+import RegisterPage from "../pages/registerPage"
+import HomePage from "../pages/homePage"
+import LoginPage from "../pages/loginPage"
+import SpecialHotPage from "../pages/specialHotPage"
+
+
+test("Register test_01",async ({page, baseURL}) => {
+
+    await page.goto(`${baseURL}route=account/register`);
+    
+    
+})
